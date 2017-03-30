2bc4866bbb613ef897632da26a52978fbc070829
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-03-30 20:19:50 +1300

    re-point sqlite paths, probably needs work in future

--- examples/dogs/db/knexfile.js
+++ examples/dogs/db/knexfile.js
@@ -5,13 +5,13 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './examples/dogs/db/dev.sqlite3'
     },
     migrations: {
-      directory: './migrations'
+      directory: './examples/dogs/db/migrations'
     },
     seeds: {
-      directory: './seeds'
+      directory: './examples/dogs/db/seeds'
     }
   },
 
