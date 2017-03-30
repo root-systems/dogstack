8165c2c3b8d4028e4c5de4edeabb0c02cb0f86a7
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-03-30 23:03:32 +1300

    wire up basic fela renderer, no use with component yet

--- examples/dogs/browser.js
+++ examples/dogs/browser.js
@@ -1,21 +1,27 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
-const { Provider } = require('react-redux')
+import { Provider as ReduxProvider } from 'react-redux'
+import { Provider as FelaProvider } from 'react-fela'
 const { browserHistory } = require('react-router')
 import { syncHistoryWithStore } from 'react-router-redux'
 
 import store from './store'
+import createRenderer from './renderer'
 import Router from './router'
 
 // store.subscribe(() => {})
 
 document.addEventListener('DOMContentLoaded', () => {
   const history = syncHistoryWithStore(browserHistory, store)
+  const renderer = createRenderer()
+  const mountNode = document.getElementById('app-styles')
 
   ReactDOM.render(
-    <Provider store={store}>
-      <Router history={history} store={store} />
-    </Provider>,
+    <ReduxProvider store={store}>
+      <FelaProvider renderer={renderer} mountNode={mountNode}>
+        <Router history={history} store={store} />
+      </FelaProvider>
+    </ReduxProvider>,
     document.querySelector('#app')
   )
 })
