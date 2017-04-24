74088a1f4a6190ee2226d76044783dd229648640
Michael Williams <michael.williams@enspiral.com>
2017-04-24 15:14:32 +1200

    setup cli system using yargs

---
+++ cli.js
@@ -0,0 +1,30 @@
+const Cli = require('yargs')
+
+const argv = Cli
+  .usage('Usage: $0 <command> [options]')
+  .commandDir('commands')
+  .options({
+    help: {
+      type: 'boolean',
+      alias: 'h',
+      description: 'Show help'
+    },
+    version: {
+      type: 'boolean',
+      description: 'Show current version'
+    },
+    cwd: {
+      type: 'string',
+      default: process.cwd(),
+      description: 'Base directory from which the relative paths are resolved'
+    }
+  })
+  // must call a subcommand, --help, or --version
+  .check(argv => argv.help || argv.version, false)
+  .help()
+  .argv
+
+if (!argv._[0] && argv.version) {
+  const pkg = require('./package.json')
+  console.log(pkg.version)
+}
