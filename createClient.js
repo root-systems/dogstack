b8c23f6ee4ad7563185374f114db1243c32e9c82
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2018-05-28 21:54:32 +1200

    use config in createClient to access api url

--- createClient.js
+++ createClient.js
@@ -5,6 +5,8 @@ const rx = require('feathers-reactive')
 const Rx = require('rxjs')
 const io = require('socket.io-client')
 
+const config = require('./config')
+
 module.exports = createClient
 
 function createClient (options) {
@@ -12,7 +14,8 @@ function createClient (options) {
     services = []
   } = options
 
-  const socket = io()
+  const apiUrl = config.api.url
+  const socket = io(apiUrl)
 
   const client = feathers()
     .configure(socketio(socket))
